4926e0b8514df7e63cd51fb581747508ff793004
RED\aguardia <aguardia@laberit.com>
2026-05-18 10:30:41 +0200

    Volcado de base de datos correcto

--- Proyecto/UC/UCConfiguracion.xaml.cs
+++ Proyecto/UC/UCConfiguracion.xaml.cs
@@ -1,19 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Win32;
+using MySql.Data.MySqlClient;
 using Proyecto.Dialogos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Proyecto.UC
 {
@@ -43,6 +33,56 @@ namespace Proyecto.UC
         {
             var dlg = _serviceProvider.GetRequiredService<AgregarEstadoMaquina>();
             dlg.ShowDialog();
+        }
+
+        private async void btnBackup_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                Filter = "Archivo de Base de Datos SQL (*.sql)|*.sql",
+                Title = "Guardar Copia de Seguridad",
+                FileName = $"AndresProyecto2_Backup_{DateTime.Now:yyyyMMdd_HHmmss}.sql"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                string rutaArchivo = dialog.FileName;
+                btnBackup.IsEnabled = false;
+
+                try
+                {
+                    await Task.Run(() => RealizarVolcadoMySQL(rutaArchivo));
+                    MessageBox.Show("El volcado de la base de datos se ha completado y guardado con éxito.", "Copia de Seguridad Exitosa", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ocurrió un error al intentar generar la copia de seguridad:\n\n{ex.Message}", "Error de Volcado", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    btnBackup.IsEnabled = true;
+                }
+            }
+        }
+
+        private void RealizarVolcadoMySQL(string rutaDestino)
+        {
+            string connectionString = "server=127.0.0.1;port=3306;database=andres_proyecto2;user=root;password=root";
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    using (MySqlBackup mb = new MySqlBackup(cmd))
+                    {
+                        cmd.Connection = conn;
+                        conn.Open();
+
+                        mb.ExportToFile(rutaDestino);
+
+                        conn.Close();
+                    }
+                }
+            }
         }
     }
 }
